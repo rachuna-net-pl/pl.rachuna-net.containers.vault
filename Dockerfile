70dca006b13ddfec4422a1dee84ffd2776de28c8
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-07-07 00:28:54 +0200

    feat: DEVOPS-199 Dodanie, aby obraz vault sam pobierał sobie klucz SSH z vault

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM ubuntu:noble
 
-LABEL Author='Maciej Rachuna'
-LABEL Application='pl.rachuna-net.containers.vault'
-LABEL Description='vault container image'
-LABEL version="${VAULT_VERSION}"
+ARG CONTAINER_VERSION="0.0.0"
+
+LABEL Author="Maciej Rachuna"
+LABEL Application="pl.rachuna-net.containers.vault"
+LABEL Description="vault container image"
+LABEL version="${CONTAINER_VERSION}"
+
+ENV DEBIAN_FRONTEND=noninteractive
 
 COPY scripts/ /opt/scripts/
 
+# Install system dependencies and ansible
 RUN apt-get update && apt-get install -y \
         curl \
         git \
@@ -22,6 +27,14 @@ RUN apt-get update && apt-get install -y \
     && apt-get upgrade -y \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/* \
-    && chmod +x /opt/scripts/*.bash
+
+# Make scripts executable
+    && chmod +x /opt/scripts/*.bash \
+
+# Create a non-root user and set permissions
+    && useradd -m -s /bin/bash user_vault \
+    && chown -R user_vault:user_vault /opt/scripts
+
+USER user_vault
 
 ENTRYPOINT [ "/opt/scripts/entrypoint.bash" ]
--- scripts/gitlab-ssh.bash
+++ scripts/gitlab-ssh.bash
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 echo -e "\033[1;33m===>\033[0m Przygotowanie kluczy SSH"
-echo -e ""
 
 if [[ -z "$GITLAB_SSH_KEY" ]]; then
-  echo "❌ Błąd: GITLAB_SSH_KEY nie jest ustawione"
-  exit 1
+  echo "⚠️ GITLAB_SSH_KEY nie jest ustawione, spróbuje pobrać"
+  if [[ -z "$VAULT_ADDR" ]]; then
+    echo "❌ Błąd: VAULT_ADDR nie jest ustawione"
+    exit 1
+  fi
+  if [[ -z "$VAULT_TOKEN" ]]; then
+    echo "❌ Błąd: VAULT_TOKEN nie jest ustawione"
+    exit 1
+  fi
+  
+  export GITLAB_SSH_KEY=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/kv-gitlab/data/pl.rachuna-net/auth/gitlab | jq -r .data.data.GITLAB_SSH_KEY)
 fi
 
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-echo "$GITLAB_SSH_KEY" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
+mkdir -p /home/user_vault/.ssh
+chmod 700 /home/user_vault/.ssh
+echo "$GITLAB_SSH_KEY" > /home/user_vault/.ssh/id_rsa
+chmod 600 /home/user_vault/.ssh/id_rsa
 
-echo "Host gitlab.com IdentityFile /root/.ssh/id_rsa StrictHostKeyChecking no" > /root/.ssh/config
+echo "Host gitlab.com IdentityFile /home/user_vault/.ssh/id_rsa StrictHostKeyChecking no" > /home/user_vault/.ssh/config
 ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
 
 echo "✅ Klucze SSH zostały pomyślnie skonfigurowane."
-echo -e ""
