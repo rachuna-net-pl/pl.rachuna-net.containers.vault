c908e4dafd298687cb3c5c1289327f4c2399c029
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-07-31 09:21:36 +0200

    fix: Analiza trivy

--- container_test.sh
+++ container_test.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 echo "🧪 Testing vault container image"
 vault --version
-
-echo "🧪 Testing working certificates ca - https://vault.rachuna-net.pl/"
-curl -s https://vault.rachuna-net.pl/
